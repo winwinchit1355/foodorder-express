8d5c2a73b14909e669d660d23d0effc7632f13db
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2026-03-27 21:59:17 +0630

    add env and usage

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.listen(5001, () => {
-    console.log("Server is running on PORT:5001");
+    console.log("Server is running on PORT:",PORT);
 });
