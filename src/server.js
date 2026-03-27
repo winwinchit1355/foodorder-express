c50bceb625f8dc9f2479994744fb7013bfcf041e
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2026-03-27 23:25:20 +0630

    create favorite api

--- src/server.js
+++ src/server.js
@@ -1,11 +1,44 @@
 import express from "express";
+import { db } from "./config/db.js";
 import { ENV } from "./config/env.js";
+import { favoritesTable } from "./db/schema.js";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
 
+app.use(express.json());
+
 app.get("/api/health",(req,res) =>{
     res.status(200).json({success: true});
+});
+
+app.post("/api/favorites", async (req,res) => {
+    try{
+        const { userId,recipeId,title,image,cookTime,servings } = req.body;
+
+        if(!userId || !recipeId || !title)
+        {
+            return res.status(400).json({
+                error: "Missing required fields"
+            });
+        }
+        const newFavorite = await db
+            .insert(favoritesTable)
+            .values({
+                userId,
+                recipeId,
+                title,
+                image,
+                cookTime,
+                servings
+            }).returning();
+
+            res.status(201).json(newFavorite[0]);
+
+    }catch (error){
+        console.log("Error adding favorite", error);
+        res.status(500).json({error: "Something went wrong"});
+    }
 });
 
 app.listen(5001, () => {
