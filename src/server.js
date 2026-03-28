c6872aba43f0be0a28e07cb2b9fd546bfd51fc25
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2026-03-28 14:03:34 +0630

    add cron.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,18 @@ import { and, eq } from "drizzle-orm";
 import { db } from "./config/db.js";
 import { ENV } from "./config/env.js";
 import { favoritesTable } from "./db/schema.js";
+import { job } from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
 
+if(ENV.NODE_ENV === "production") job.start();
+
 app.use(express.json());
+
+app.get("/api/health",(req,res) =>{
+    res.status(200).json({success: true});
+});
 
 app.get("/api/favorites/:userId", async (req, res) => {
     try {
