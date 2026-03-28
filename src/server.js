70e8b3661d71c4ddc087c32ea019469615e7dd9a
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2026-03-28 12:53:01 +0630

    favorite get and delete api

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { and, eq } from "drizzle-orm";
 import { db } from "./config/db.js";
 import { ENV } from "./config/env.js";
 import { favoritesTable } from "./db/schema.js";
@@ -8,16 +9,23 @@ const PORT = ENV.PORT || 5001;
 
 app.use(express.json());
 
-app.get("/api/health",(req,res) =>{
-    res.status(200).json({success: true});
+app.get("/api/favorites/:userId", async (req, res) => {
+    try {
+        const {userId} = req.params;
+        const userFavorites = await db.select().from(favoritesTable)
+            .where(eq(favoritesTable.userId,userId));
+        res.status(200).json(userFavorites);
+    } catch (error) {
+        console.log("Error removing a favorite", error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
 });
 
-app.post("/api/favorites", async (req,res) => {
-    try{
-        const { userId,recipeId,title,image,cookTime,servings } = req.body;
+app.post("/api/favorites", async (req, res) => {
+    try {
+        const { userId, recipeId, title, image, cookTime, servings } = req.body;
 
-        if(!userId || !recipeId || !title)
-        {
+        if (!userId || !recipeId || !title) {
             return res.status(400).json({
                 error: "Missing required fields"
             });
@@ -33,14 +41,30 @@ app.post("/api/favorites", async (req,res) => {
                 servings
             }).returning();
 
-            res.status(201).json(newFavorite[0]);
+        res.status(201).json(newFavorite[0]);
 
-    }catch (error){
+    } catch (error) {
         console.log("Error adding favorite", error);
-        res.status(500).json({error: "Something went wrong"});
+        res.status(500).json({ error: "Something went wrong" });
+    }
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+    try {
+        const { userId, recipeId } = req.params;
+        console.log(userId, recipeId);
+        await db
+            .delete(favoritesTable)
+            .where(
+                and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+            );
+        res.status(200).json({ message: "Favorite removed successfully" });
+    } catch (error) {
+        console.log("Error removing a favorite", error);
+        res.status(500).json({ error: "Something went wrong" });
     }
 });
 
 app.listen(5001, () => {
-    console.log("Server is running on PORT:",PORT);
+    console.log("Server is running on PORT:", PORT);
 });
